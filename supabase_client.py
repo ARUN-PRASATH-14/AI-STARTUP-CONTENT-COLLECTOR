2c0c8c3bb0eb0b2b6a403685241926ef8b98d3f7
ARUN PRASATH C <133093102+ARUN-PRASATH-14@users.noreply.github.com>
2026-06-13 11:26:35 +0530

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,10 +1,10 @@
-from supabase import create_client
-from dotenv import load_dotenv
 import os
+from supabase import create_client
 
-load_dotenv()
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
 
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+if not url or not key:
+    raise ValueError("Missing Supabase credentials")
 
 supabase = create_client(url, key)
