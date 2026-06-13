66b34bc3df8c9264556d4bff8e82e46d5913f74e
ARUN PRASATH C <133093102+ARUN-PRASATH-14@users.noreply.github.com>
2026-06-13 11:26:04 +0530

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,10 +1,10 @@
-import os
 from supabase import create_client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
-
-if not url or not key:
-    raise ValueError("Missing Supabase environment variables")
 
 supabase = create_client(url, key)
